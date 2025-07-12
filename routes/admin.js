88f83ad7c85d825c75af629a5903bc36f8674ee7
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-07-13 00:56:21 +0700

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../utils/db');
+const db = require('../db');
 const { isAdmin } = require('../middleware/auth');
 
 // Dashboard
